49cb71c0b77b26f17f75719701b0505799374f57
ingmarvisser <ingmarvisser@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-25 13:30:22 +0000

    First version of gazepath
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gazepatterns@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/gazepath/R/simplify.R
@@ -0,0 +1,45 @@
+simplify <-
+function(classification, x, y, Hz, fixations_only = FALSE){
+  class <- rle(classification)
+  simple <- data.frame(class$values, class$lengths, 
+                       c(1, cumsum(class$lengths) + 1)[-(length(class$values) + 1)], 
+                       cumsum(class$lengths))
+  if(fixations_only == FALSE){
+    if(length(which(class$values == 'f')) > 0){
+      x_start <- y_start <- x_end <- y_end <- mean_x <- mean_y <- POGvar <- numeric()
+      for(i in 1:dim(simple)[1]){
+        x_start <- c(x_start, x[simple[i, 3]])
+        y_start <- c(y_start, y[simple[i, 3]])
+        x_end <- c(x_end, x[simple[i, 4]])
+        y_end <- c(y_end, y[simple[i, 4]])
+        mean_x <- c(mean_x, mean(x[simple[i,3] : simple[i,4]]))
+        mean_y <- c(mean_y, mean(y[simple[i,3] : simple[i,4]]))
+        POGvar <- c(POGvar, mean(as.matrix(dist(cbind(c(mean_x[length(mean_x)], x[simple[i,3] : simple[i,4]]), c(mean_y[length(mean_y)], y[simple[i,3] : simple[i,4]]))))[-1,1]))
+      }
+      simple <- data.frame(class$values, class$lengths * (1000/Hz), 
+                           c(1, cumsum(class$lengths * (1000/Hz)) + 1)[-(length(class$values) + 1)], 
+                           cumsum(class$lengths * (1000/Hz)), x_start, y_start, x_end, y_end, mean_x, mean_y, POGvar)
+      names(simple)[1:4] <- c('Value', 'Dur', 'Start', 'End')
+    }
+  } else {
+    if(length(which(class$values == 'f')) > 0){
+      mean_x <- mean_y <- POGvar_px <- numeric()
+      for(i in which(simple[,1] == 'f')){
+        mean_x <- c(mean_x, mean(x[simple[i,3] : simple[i,4]]))
+        mean_y <- c(mean_y, mean(y[simple[i,3] : simple[i,4]]))
+        POGvar_px <- c(POGvar_px, mean(dist(cbind(x[simple[i,3] : simple[i,4]], y[simple[i,3] : simple[i,4]]))))
+      }
+      simple_by <- cbind(mean_x, mean_y)
+      simple <- data.frame(class$values, class$lengths * (1000/Hz), 
+                           c(1, cumsum(class$lengths * (1000/Hz)) + 1)[-(length(class$values) + 1)], 
+                           cumsum(class$lengths * (1000/Hz)))
+      names(simple)[1:4] <- c('Value', 'Dur', 'Start', 'End')
+      simple <- data.frame(simple[which(simple[,1] == 'f'),], simple_by)
+    }
+  }
+  # Remove NA values
+  if(length(which(is.na(simple[,1]))) != 0){
+    simple <- simple[-which(is.na(simple[,1])),]
+  }
+  return(simple)
+}
