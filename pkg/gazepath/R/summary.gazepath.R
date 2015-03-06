f854bca9f13a49f56713c03c96739c10618b9fa2
dvrensw <dvrensw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-06 15:11:53 +0000

    fixed errors from previous update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gazepatterns@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gazepath/R/summary.gazepath.R
+++ pkg/gazepath/R/summary.gazepath.R
@@ -6,15 +6,22 @@ function(object, ..., complete_only = FALSE){
     l <- length(which(sim[,1] == 'f'))
     if(l != 0){
       if(complete_only == TRUE){
-        index <- sort(c(complete(sim, 'f'), complete(sim, 's')))
-        if(length(index) != 0){
-          output <- rbind(output, cbind(sim[index, c(1:2, 9:11)], 1:length(index), i))
-        } 
+        if(length(which(sim[,1] == 's')) != 0){
+          index <- sort(c(complete(sim, 'f'), complete(sim, 's')))
+          if(length(index) != 0){
+            output <- rbind(output, cbind(sim[index, c(1:2, 9:11)], 1:length(index), i))
+          }
+        }         
       } else {
         output <- rbind(output, cbind(sim[sim[,1] == 'f', c(1:2, 9:11)], 1:l, i))
       }
     }
   }
+  if(length(output) == 0){
+    print('There were no fixations or saccades classified, probably data quality of this particpant is very low')
+    output <- data.frame(matrix(NA, 1, 7))
+    names(output)[3:5] <- c('mean_x', 'mean_y', 'POGvar')
+  } 
   names(output)[c(1:2, 6:7)] <- c('Value', 'duration', 'order', 'trial')
   row.names(output) <- 1:dim(output)[1]
   return(output)
