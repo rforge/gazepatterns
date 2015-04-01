e8e3ca40d6dc94515f5099177b68646bc053457a
dvrensw <dvrensw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-01 12:54:43 +0000

    Added time stamps to summary output
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gazepatterns@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gazepath/R/summary.gazepath.R
+++ pkg/gazepath/R/summary.gazepath.R
@@ -9,11 +9,11 @@ function(object, ..., complete_only = FALSE){
         if(length(which(sim[,1] == 's')) != 0){
           index <- sort(c(complete(sim, 'f'), complete(sim, 's')))
           if(length(index) != 0){
-            output <- rbind(output, cbind(sim[index, c(1:2, 9:11)], 1:length(index), i))
+            output <- rbind(output, cbind(sim[index, c(1:4, 9:11)], 1:length(index), i))
           }
         }         
       } else {
-        output <- rbind(output, cbind(sim[sim[,1] == 'f', c(1:2, 9:11)], 1:l, i))
+        output <- rbind(output, cbind(sim[sim[,1] == 'f', c(1:4, 9:11)], 1:l, i))
       }
     }
   }
@@ -22,7 +22,7 @@ function(object, ..., complete_only = FALSE){
     output <- data.frame(matrix(NA, 1, 7))
     names(output)[3:5] <- c('mean_x', 'mean_y', 'POGvar')
   } 
-  names(output)[c(1:2, 6:7)] <- c('Value', 'duration', 'order', 'trial')
+  names(output)[c(1:4, 8:9)] <- c('Value', 'Duration', 'Start', 'End', 'Order', 'Trial')
   row.names(output) <- 1:dim(output)[1]
   return(output)
 }
