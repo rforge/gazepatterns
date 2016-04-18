5acf02edebc925f0fb3ff7c66b4ed8b2f1e2bb09
dvrensw <dvrensw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-18 12:11:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gazepatterns@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gazepath/R/comhull.R
+++ pkg/gazepath/R/comhull.R
@@ -7,7 +7,7 @@ comhull <- function(d, classification, dat_x, dat_y, in_thres, Hz = Hz){
     fix2 <- which(d$index == 'fixation')[count]
     cvhull <- chull(cbind(dat_x, dat_y)[d[fix,3] : d[fix,4],])
     POLY_FIX <- cbind(dat_x[d[fix,3] : d[fix,4]][cvhull], dat_y[d[fix,3] : d[fix,4]][cvhull])
-    PNT <- sum(tail(pnt.in.poly(cbind(dat_x, dat_y)[d[fix2,3] : d[fix2,4],], POLY_FIX)[,3], 100 * (Hz / 1000)))
+    PNT <- sum(pnt.in.poly(cbind(dat_x, dat_y)[d[fix2,3] : d[fix2,4],], POLY_FIX)[,3])
     ## If fixations have the same location and are within interpolation limit combine them...
     if(PNT > 0 & (d[fix,3] - d[fix2,4]) < in_thres * (Hz / 1000)){
       classification[d[fix2,3] : d[fix,4]] <- 'fixation'
@@ -30,3 +30,7 @@ comhull <- function(d, classification, dat_x, dat_y, in_thres, Hz = Hz){
   }
   return(list(classification, dat_x, dat_y))
 }
+
+
+
+
