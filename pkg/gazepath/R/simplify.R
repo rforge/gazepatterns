dca23edb357730218b12acdd088d9b6f7f3bb3c9
dvrensw <dvrensw@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-01 10:24:25 +0000

    implement suggestions after review
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gazepatterns@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gazepath/R/simplify.R
+++ pkg/gazepath/R/simplify.R
@@ -13,16 +13,16 @@ function(classification, x, y, Hz, D, width_px, width_mm, extra, extra_var){
         y_end <- c(y_end, y[simple[i, 4]])
         mean_x <- c(mean_x, mean(x[simple[i,3] : simple[i,4]]))
         mean_y <- c(mean_y, mean(y[simple[i,3] : simple[i,4]]))
-        POGvar <- c(POGvar, mean(as.matrix(dist(cbind(c(mean_x[length(mean_x)], x[simple[i,3] : simple[i,4]]), c(mean_y[length(mean_y)], y[simple[i,3] : simple[i,4]]))))[-1,1]))
+        POGvar <- c(POGvar, sqrt(mean(as.matrix(dist(cbind(c(mean_x[length(mean_x)], x[simple[i,3] : simple[i,4]]), c(mean_y[length(mean_y)], y[simple[i,3] : simple[i,4]]))))[-1,1])))
       }
-      ## Calculate saccade amplitude and transform POGvar from pixels to degrees of visual angle
+      ## Calculate saccade amplitude and transform POGsd from pixels to degrees of visual angle
       ss <- which(class$values == 's')
       POGvar[ss] <- sqrt((x_start[ss] - x_end[ss]) ^ 2 + (y_start[ss] - y_end[ss]) ^ 2)
-      POGvarSacAmp <- atan((POGvar / 2) / mean(D, na.rm = T)) * (180 / pi) * (width_mm / width_px) * 2
+      POGsdSacAmp <- atan((POGvar / 2) / mean(D, na.rm = T)) * (180 / pi) * (width_mm / width_px) * 2
       
       simple <- data.frame(class$values, class$lengths * (1000/Hz), 
                            c(1, cumsum(class$lengths * (1000/Hz)) + 1)[-(length(class$values) + 1)], 
-                           cumsum(class$lengths * (1000/Hz)), x_start, y_start, x_end, y_end, mean_x, mean_y, POGvarSacAmp)
+                           cumsum(class$lengths * (1000/Hz)), x_start, y_start, x_end, y_end, mean_x, mean_y, POGsdSacAmp)
       names(simple)[1:4] <- c('Value', 'Dur', 'Start', 'End')
       if(!is.null(extra_var)){
         for(i in 1:length(extra_var)){
